066f23d0e5b07d0723214e13cf3a0bf937533896
slimmjimmy <jakefalagrady@gmail.com>
2023-03-14 17:00:21 -0600

    Cart Added

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_jfalagra.Models;
+
+namespace Mission09_jfalagra.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
